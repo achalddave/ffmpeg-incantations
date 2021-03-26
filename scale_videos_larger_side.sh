53f8dfad8391925015b8bba15dd43acd8f2f3806
Achal Dave <achalddave@gmail.com>
2021-03-26 11:15:51 -0400

    Update scale_videos_larger_side.sh

--- scale_videos_larger_side.sh
+++ scale_videos_larger_side.sh
@@ -1,11 +1,17 @@
-# Scale a directory of videos to be at most of a certain dimension.
+#!/bin/bash
+# Scale a directory of videos so the max dimension is below a certain size.
+
+set -e
 
 max_size=720
 for i in *.mp4 ; do
   w=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 ${i})
   h=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 ${i})
   if [[ $w -gt $max_size ]] && [[ $h -gt $max_size ]] ; then
-    # https://stackoverflow.com/a/42024302
-    ffmpeg -i ${i} -vf "scale=$max_size:$max_size:force_original_aspect_ratio=decrease" ${i%.mp4}_reduced.mp4
+    # We can't use "force_original_aspect_ratio" like here:
+    # https://stackoverflow.com/a/42024302 because it results in output sizes
+    # not divisible by 2, which causes some formats to choke.
+    # Instead, we use this: https://stackoverflow.com/a/23662884
+    ffmpeg -i ${i} -vf "scale='if(gt(iw,ih),${max_size},trunc(oh*a/2)*2)':'if(gt(iw,ih),trunc(ow/a/2)*2,${max_size})" ${i%.mp4}_reduced.mp4
   fi
 done
